8b0777f59d01d3f80803fc97598589077f4bea9d
rileyherman <riley.herman@fcc-fac.ca>
2021-10-08 22:13:09 -0600

    well I did something and now I'm tired so sue me

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -3,7 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-content',
   template: `
-    <div class="grid grid-cols-6 p-8">
+  <!-- Task 1 - Aria-l bold
+    ARIA elements are 
+    Why would you do this?
+    For accessibility reasons, of course!
+    aria-hidden removes elements from the accessibility tree that are extraneous or irrelevant. 
+    Unfortunately, here we're removing the bulk of our site from the accessibility tree, which 
+    is not a good idea.
+
+    The general advice is to use HTML5 attributes with built in aria elements. I know we all just 
+    use <div> for everything
+
+    Remove the offending attribute, please!
+
+    Reference: 
+    https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA
+    https://developer.mozilla.org/en-US/docs/Web/Accessibility/Keyboard-navigable_JavaScript_widgets
+    https://w3c.github.io/using-aria/
+
+    Next task in ../../../main.ts
+   -->
+    <div class="grid grid-cols-6 p-8" aria-hidden="true">
       <div class="col-start-2 col-span-4">
         <ng-content></ng-content>
       </div>
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,13 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  // Task 2 - Just because you can, doesn't mean you should
+  // You're developers - I don't need to explain to you why this is a dumb piece of code. 
+  // BUT I do want you to run Lighthouse with this still here and see if it gets detected. 
+  // Note that if you haven't done Task 1, that will be detected by Lighthouse.
+  
+  // Next task in ./styles.css
+  document.addEventListener('keydown', function (e) {
+    e.preventDefault();
+  });
