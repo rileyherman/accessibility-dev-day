a6fc60a91caa151fd2f414618a836559b130aa86
rileyherman <riley.herman@fcc-fac.ca>
2021-10-15 12:57:23 -0600

    remove accessiility page

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # AccessibilityDevDay
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.1.
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './routes/about-page/about-page.component';
-import { AccessibilityPageComponent } from './routes/accessibility-page/accessibility-page.component';
 import { HomePageComponent } from './routes/home-page/home-page.component';
 import { TeamPageComponent } from './routes/team-page/team-page.component';
 
@@ -13,10 +12,6 @@ const routes: Routes = [
   {
     path: 'team/:teamId',
     component: TeamPageComponent,
-  },
-  {
-    path: 'accessibility',
-    component: AccessibilityPageComponent,
   },
   {
     path: 'about',
