8ace80079b05578f2748a1323935b9da12eb398a
rileyherman <riley.herman@fcc-fac.ca>
2021-11-25 13:39:48 -0600

    keyboard nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,14 +6,14 @@ import { Router } from '@angular/router';
   template: `
     <header class="flex items-center justify-between p-6 bg-gray-100 text-gray-900 border border-l-0 border-t-0 border-r-0 shadow">
       <div class="flex items-center text-lg">
-        <span [class.invisible]="!back" (click)="back()" class="material-icons mr-4 cursor-pointer">arrow_back</span>
+        <span tabindex="0" role="link" [class.invisible]="!back" (click)="back()" class="material-icons mr-4 cursor-pointer">arrow_back</span>
         <h3>Is the replay worth it?</h3>
       </div>
-      <div class="flex items-center hidden xl:block text-lg">
-        <a class="cursor-pointer mr-4" (click)="about()">About</a>
-      </div>
+      <nav class="flex items-center hidden xl:block text-lg">
+        <a class="cursor-pointer mr-4" tabindex="0" href="/about">About</a>
+      </nav>
       <nav class="flex items-center xl:hidden">
-        <span (click)="about()" class="material-icons cursor-pointer mr-4">help_outline</span>
+        <a tabindex="0" href="/about"><span class="material-icons cursor-pointer mr-4">help_outline</span></a>   
       </nav>
     </header>
   `
@@ -26,9 +26,5 @@ export class HeaderComponent {
 
   about = () => {
     this.router.navigate(['about']);
-  }
-
-  accessibility = () => {
-    this.router.navigate(['accessibility']);
   }
 }
